2991c6a9290c696849552603711746976d6e3009
Sebastien Ooteman <sebas-gamer@hotmail.com>
2020-11-26 11:16:21 +0100

    week5 JS opschooning en comment

--- whack-a-mole.js
+++ whack-a-mole.js
@@ -1,14 +1,8 @@
 const square = document.querySelectorAll('.square');	// Kijkt naar de class om te weten wat gebruikt wordt.
 const mole = document.querySelectorAll('.mole');	// Kijkt naar class mol om te weten wat de mol is.
 const timeLeft = document.querySelector('#time-left');	// Kijkt naar de id en de ingevoerde waarde in de HTML.
-let score = document.querySelector('#score'); // Wordt gebruikt om de score in de HTML te plaatsen
-var hitscore = 0;
-let hamer = document.getElementById("dehamer");
-
-document.getElementsByTagName("body")[0].onmousemove = function(e){
-	hamer.style.top = e.clientY+"px";
-	hamer.style.left = e.clientX+2+"px";
-}
+let score = document.querySelector('#score'); // Wordt gebruikt om de score in de HTML te plaatsen.
+var hitscore = 0;	// Dit is een defaulte waarde die ergens anders een speciale waarde krijgt.
 
 let result = 0	// De begin waarde voor de score.
 let currentTime = timeLeft.textContent 	// Dit toont de tijd waarde in de HTML twerwijl het aftelt.
@@ -27,40 +21,30 @@ function randomSquare() {	// Deze functie regelt het willekeurige gespring van d
 	let randomPosition = square[Math.floor(Math.random() * 15)]	// Dit zorgt ervoor dat de mol op 1 van de verschillende plekken kan zijn.
 	randomPosition.classList.add('mole'+kans)	// Dit zorgt ervoor dat de afbeelding van de mol geplaatst wordt in het vakje waar die nu is.
 
-	switch (kans){	// bij deze switch worden de punten toegekend op basis van 'bonus'; gewone mol is 1 punt, special is 5 punten
+	switch (kans){	// bij deze switch worden de punten toegekend op basis van 'bonus'; gewone mol is 1 punt, special is 5 punten.
 		case 0: case 1: case 2: case 3:
 			hitscore = 1;
-			break;			// bij deze switch worden de punten toegekend op basis van 'bonus'; gewone mol is 1 punt, special is 5 punten
+			break;			// bij deze switch worden de punten toegekend op basis van 'bonus'; gewone mol is 1 punt, special is 5 punten.
 		case 4:
 			hitscore = 3;	
 			break; 
 		case 5: 	
 			hitscore = 5;
-			break;		// bij deze switch worden de punten toegekend op basis van 'bonus'; gewone mol is 1 punt, special is 5 punten	
+			break;		// bij deze switch worden de punten toegekend op basis van 'bonus'; gewone mol is 1 punt, special is 5 punten.
 	}
-/*	console.log(kans);
-	kans = Math.floor(Math.random() * 3)
-	if(kans == 1){
-		let randomPosition2 = square[Math.floor(Math.random() * 15)]
-		randomPosition2.classList.add('mole'+kans)
-		hitPosition2 = randomPosition2.id
-	}	*/
 	hitPosition = randomPosition.id	// Dit is zodat de hit/klik positie hetzelfde zal zijn als de willekeurige positie.
 }
 
 square.forEach(id => {	// Dit is de variabele voor de mol z'n (huidige) positie.
 	id.addEventListener('click', () => {	// Luistert naar het klik evenement.
-		var audio = document.getElementById("audio");
-		audio.play();
-
-		//		console.log(id.id +"<<>>"+hitPosition);
+		var audio = document.getElementById("audio");	// Zorgt ervoor dat er een audio bestand aan geroepen wordt.
+		audio.play();	// Dit zorgt ervoor dat er een audio bestand afgespeeld wordt.
 		if(id.id === hitPosition || id.id === hitPosition2){	// Als de klik op de zelfde plek is als de mol op dat moment dan telt het als een succesvolle mep.
 			result = result + hitscore	// Telt er elke keer 1 bij als je succesvol op de mol klikt.
 			score.textContent = result	// Zet de huidige score in de in de HTML veld.
 			hitPosition=null	// Betekent dat de hitPosition nog geen waarde heeft.
 			hitPosition2=null	// Betekent dat de hitPosition nog geen waarde heeft.
 		}
-//		console.log("aantalpunten:"+result);
 	})
 })
 
